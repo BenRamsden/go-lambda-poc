cef6a93ee97c9fdfd7062932d78d5bc79ddd65bd
Ben Ramsden <benramsdensii@gmail.com>
2024-02-20 12:35:18 +0000

    feat: go/ts test equality

--- cmd/api/lambda_main.go
+++ cmd/api/lambda_main.go
@@ -4,24 +4,19 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/getsentry/sentry-go"
-	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 	"github.com/jugo-io/go-poc/api/auth"
 	"github.com/jugo-io/go-poc/api/dynamo"
 	"github.com/jugo-io/go-poc/api/graph"
 	"github.com/jugo-io/go-poc/api/service"
 	"github.com/vektah/gqlparser/v2/gqlerror"
+	"log"
 )
 
 var ginLambda *ginadapter.GinLambda
@@ -31,23 +26,23 @@ func init() {
 
 	r := gin.Default()
 
-	if err := sentry.Init(sentry.ClientOptions{
-		Dsn:              os.Getenv("SENTRY_DSN"),
-		Debug:            false,
-		AttachStacktrace: true,
-	}); err != nil {
-		log.Printf("Sentry initialization failed: %v\n", err)
-	} else {
-		log.Println("Sentry initialized")
-	}
-	r.Use(sentrygin.New(sentrygin.Options{}))
+	//if err := sentry.Init(sentry.ClientOptions{
+	//	Dsn:              os.Getenv("SENTRY_DSN"),
+	//	Debug:            false,
+	//	AttachStacktrace: true,
+	//}); err != nil {
+	//	log.Printf("Sentry initialization failed: %v\n", err)
+	//} else {
+	//	log.Println("Sentry initialized")
+	//}
+	//r.Use(sentrygin.New(sentrygin.Options{}))
 	// Sentry Flush with lambda?
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
 
 	repo := dynamo.NewFromEnv()
 	assets := service.NewAssetService(repo)
@@ -63,7 +58,7 @@ func init() {
 		return err
 	})
 
-	r.GET("/playground", gin.WrapH(playground.Handler("GraphQL playground", "/graphql")))
+	//r.GET("/playground", gin.WrapH(playground.Handler("GraphQL playground", "/graphql")))
 	r.POST("/graphql", auth.EnsureValidToken(), gin.WrapH(graphSrv))
 
 	ginLambda = ginadapter.New(r)
